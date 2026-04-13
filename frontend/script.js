e1f006ba5c6d9407cf29fa1f1c80ec2d890e01d1
Jake Gaylord <jgaylord@bowdoin.edu>
2026-04-13 16:37:55 -0400

    added the sign up pop up and made it so only login OR sign up can be open at one time

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,18 @@
-function openForm() {
+function openLoginForm() {
+    closeSignUpForm();
     document.getElementById("loginForm").style.display = "block";
   }
   
-  function closeForm() {
+  function closeLoginForm() {
     document.getElementById("loginForm").style.display = "none";
   }
+
+function openSignUpForm() {
+    closeLoginForm();
+    document.getElementById("signupForm").style.display = "block";
+  }
+  
+  function closeSignUpForm() {
+    document.getElementById("signupForm").style.display = "none";
+  }
+

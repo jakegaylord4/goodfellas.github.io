0496bcd30f21cb51054bfb196b5079aa437cf4bf
Henry Smith <hssmith2704@gmail.com>
2026-04-27 09:48:59 -0400

    Added venv to gitignore

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,49 +1,49 @@
 function openLoginForm() {
-    closeSignUpForm();
-    document.getElementById("loginForm").style.display = "block";
-  }
-  
-  function closeLoginForm() {
-    document.getElementById("loginForm").style.display = "none";
-  }
+  closeSignUpForm();
+  document.getElementById("loginForm").style.display = "block";
+}
+
+function closeLoginForm() {
+  document.getElementById("loginForm").style.display = "none";
+}
 
 function openSignUpForm() {
-    closeLoginForm();
-    document.getElementById("signupForm").style.display = "block";
-  }
-  
-  function closeSignUpForm() {
-    document.getElementById("signupForm").style.display = "none";
-  }
+  closeLoginForm();
+  document.getElementById("signupForm").style.display = "block";
+}
+
+function closeSignUpForm() {
+  document.getElementById("signupForm").style.display = "none";
+}
 
 let login = () => {
-    // TODO: Implement login logic
-    const email = document.getElementById("email-login").value;
-    const password = document.getElementById("psw-login").value;
-    console.log("Login clicked", email, password);
+  // TODO: Implement login logic
+  const email = document.getElementById("email-login").value;
+  const password = document.getElementById("psw-login").value;
+  console.log("Login clicked", email, password);
 }
 
 let signUp = () => {
-    // TODO: Implement sign up logic
-    const firstName = document.getElementById("firstname-signup").value;
-    const lastName = document.getElementById("lastname-signup").value;
-    const email = document.getElementById("email-signup").value;
-    const password = document.getElementById("psw-signup").value;
-    console.log("Sign up clicked", firstName, lastName, email, password);
+  // TODO: Implement sign up logic
+  const firstName = document.getElementById("firstname-signup").value;
+  const lastName = document.getElementById("lastname-signup").value;
+  const email = document.getElementById("email-signup").value;
+  const password = document.getElementById("psw-signup").value;
+  console.log("Sign up clicked", firstName, lastName, email, password);
 }
 
 function openLogoutForm() {
-    document.getElementById("logoutForm").style.display = "block";
-  }
-  
-  function closeLogoutForm() {
-    document.getElementById("logoutForm").style.display = "none";
-  }
+  document.getElementById("logoutForm").style.display = "block";
+}
+
+function closeLogoutForm() {
+  document.getElementById("logoutForm").style.display = "none";
+}
 
 const fileInput = document.getElementById('service_image');
 if (fileInput) {
-    fileInput.addEventListener('change', function () {
-        const fileName = this.files[0] ? this.files[0].name : '';
-        document.getElementById('file-name').innerText = fileName;
-    });
+  fileInput.addEventListener('change', function () {
+    const fileName = this.files[0] ? this.files[0].name : '';
+    document.getElementById('file-name').innerText = fileName;
+  });
 }
